67709c7b5bdf581dd6bc0dea0651955ac44c7321
mohammed <mohammed.bouharrada@gmail.com>
2018-12-28 16:20:09 +0100

    initial commit
    first push

---
+++ src/App.js
@@ -0,0 +1,68 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+
+let defaultTextColor = '#fff';
+let defaultStyle ={
+  color : defaultTextColor
+};
+
+class Aggregate extends Component{
+  render() {
+    return(
+      <div className="aggregate" style={{...defaultStyle,width:"40%",display:'inline-block'}}>
+      <h2 style={{...defaultStyle,width:"40%",display:'inline-block'}}>number text</h2>
+      </div>
+    );
+  }
+}
+
+class Filter extends Component{
+  render(){
+    return(
+      <div>
+        <img/>
+        <input type ="text"/>
+      </div>
+
+    );
+  }
+}
+
+
+class Playlist extends Component{
+  render(){
+    return(
+      <div style={{...defaultStyle, width:'25%', display :'inline-Block'}}> 
+        <img/>
+        <h3>Playlist name</h3>
+        <ul>
+          <li>List 1</li>
+          <li>List 2</li>
+          <li>List 3</li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Title</h1>
+        <Aggregate  />
+        <Aggregate  />
+        <Filter/>
+        <Playlist/>
+        <Playlist/>
+        <Playlist/>
+      </div>
+    );
+  }
+}
+
+export default App;
